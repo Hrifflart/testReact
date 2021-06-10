602776e0a1a2e69e064467b8f10579cf110b78a8
Hrifflart <83953668+Hrifflart@users.noreply.github.com>
2021-06-10 13:52:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom'
-import App from './App';
-import Form from './Formu';
-
-ReactDOM.render(<App />, document.getElementById('root'))
