48c22fbfd5a9aef9abfc9d92e7fb6ea37000ed47
Hrifflart <83953668+Hrifflart@users.noreply.github.com>
2021-06-10 11:05:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import App from './App';
 import Form from './Formu';
-import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
