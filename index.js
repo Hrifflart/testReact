d82a5a433fca96ff8fc9ffbc8264a99662339646
Hrifflart <83953668+Hrifflart@users.noreply.github.com>
2021-06-10 10:49:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
-import App from './App';
-import Form from './Formu';
-import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        Name: '',
+        Start_hour: '',
+        End_hour: '',
+        Topic: '',
+        Link: '',
+      }
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(e) {
+      const n = e.target.name
+      this.setState({
+        [n]: e.target.value
+      })
+    }
+
+    handleSubmit (e) {
+      e.preventDefault()
+      const data = JSON.stringify(this.state)
+      this.setState({
+        Name: '',
+        Start_hour: '',
+        End_hour: '',
+        Topic: '',
+        Link: '',
+      })
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Meetings : </h1>
+          
+          <div>
+            <label htmlFor="name">Name </label>
+            <input type="text" value={this.state.name} onChange={this.handleChange} id="name" name="Name"/><br></br><br></br>
+          </div>
+
+          <div>
+            <label htmlFor="start_hour">Start hour </label>
+            <input type="text" value={this.state.start_hour} onChange={this.handleChange} id="start_hour" name="Start_hour"/><br></br><br></br>
+          </div>
+          
+          <div>
+            <label htmlFor="end_hour">End hour </label>
+            <input type="text" value={this.state.end_hour} onChange={this.handleChange} id="end_hour" name="End_hour"/><br></br><br></br>
+          </div>
+
+          <div>
+            <label htmlFor="topic">Topic </label>
+            <input type="text" value={this.state.topic} onChange={this.handleChange} id="topic" name="Topic"/><br></br><br></br>
+          </div>
+
+          <div>
+            <label htmlFor="link">Link </label>
+            <input type="text" value={this.state.link} onChange={this.handleChange} id="link" name="Link"/><br></br><br></br>
+          </div>
+
+          <div>
+            <input type="submit" value="save" />
+          </div>
+          
+          {JSON.stringify(this.state)}
+        </div>
+      )
+    }
+  }
+
+  ReactDOM.render(<App />, document.getElementById('root'))
